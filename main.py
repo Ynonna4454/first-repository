5a525077e9b55e22ec49574a30f244c1e3c9696f
Ynonna4454/ <tsybenova.anna@yandex.ru>
2025-05-13 21:27:17 +0800

    name

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-def area_circle(r):
-    S = 3.14 * r**2
-    return S
-
-def area_square(a):
-    S s = a**2
-    return S_s
-
-
-def area_traprzoid(a, b, h):
-   S_t = 0.5*(a + b) * h
-   return S_t
+def hello(name):
+    print(f"Hello {name}")
